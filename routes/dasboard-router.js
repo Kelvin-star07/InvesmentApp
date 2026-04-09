81dcf12291804b97a3aa230670cd91b8ad3cd6b7
kelvinkodad07 <kelvinkodaddiaz@gmail.com>
2026-04-08 22:01:14 -0400

    Editando import del dasboard para el deployment

--- routes/dasboard-router.js
+++ routes/dasboard-router.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import {GetIndex} from '../controllers/DasboardController.js';
-import IsAuth from '../middlewares/IsAuth.js';
+import isAuth from '../middlewares/isAuth.js';
 
 const router = express.Router();
 
 // Dashboard route
-router.get('/dasboard',IsAuth,GetIndex);
+router.get('/dasboard',isAuth,GetIndex);
 
 
 export default router;
